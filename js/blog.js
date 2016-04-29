66cfe3dd24c5ef95813d2f74254c2fad0fa4c092
Dorothy Chang <dorothyichang@gmail.com>
2016-04-29 03:46:01 -0400

    added blog.html

---
+++ js/blog.js
@@ -0,0 +1,20 @@
+var Firebase = require("firebase");
+
+var ref = new Firebase("https://crackling-heat-5677.firebaseio.com/");
+
+ref.authWithPassword({
+  email    : "dorothyichang@gmail.com",
+  password : "Starbucks123"
+}, function(error, authData) { /* Your Code */ }, {
+  remember: "sessionOnly"
+});
+
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
