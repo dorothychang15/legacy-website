ed52fe5b10e66ac0b05d395c4f344e08eecb8a15
Dorothy Chang <dorothyichang@gmail.com>
2016-04-30 16:23:15 -0400

    blog posts ordered by newest first

--- js/blog-login.js
+++ js/blog-login.js
@@ -2,30 +2,52 @@ require(['https://cdn.firebase.com/js/client/2.4.2/firebase.js'], function (fire
 
 var ref = new Firebase("https://crackling-heat-5677.firebaseio.com/");
 
-ref.authWithPassword({
-  email    : "dorothyichang@gmail.com",
-  password : "Starbucks123"
-}, function(error, authData) { }, {
-  remember: "sessionOnly"
-});
+// var userField = $("#loginemail");
+// var passField = $("#loginpassword");
+
+// $("#loginbtn").on("click", function () {
+//   var user = new String(userField.val()).valueOf();
+//   var password = new String(passField.val()).valueOf();
+//   if (user === (new String("dorothyichang@gmail.com").valueOf()) && password === (new String("password").valueOf())) {
+//     console.log("logged in!");
+
+//     ref.authWithPassword({
+//       email    : "dorothyichang@gmail.com",
+//       password : "password"
+//     }, function(error, authData) { }, {
+//       remember: "sessionOnly"
+//     });
+
+//     $("#logincontainer")[0].style.visibility="hidden";
+//     $("#postcontainer")[0].style.visibility="visible";
+//     e.preventDefault();
+//   }
+// });
+
 
 $("#loginbtn").on("click", function (e) {
-	$("#logincontainer")[0].style.visibility="hidden";
-	$("#postcontainer")[0].style.visibility="visible";
-	e.preventDefault();
+  $("#logincontainer")[0].style.visibility="hidden";
+  $("#postcontainer")[0].style.visibility="visible";
+  e.preventDefault();
+  ref.authWithPassword({
+      email    : "dorothyichang@gmail.com",
+      password : "password"
+    }, function(error, authData) { }, {
+      remember: "sessionOnly"
+    });
 });
 
 var titleField = $("#titleinput");
 var textField = $("#textinput");
 
 $("#postbtn").on("click", function (e) {
-	var title = titleField.val();
-	var text = textField.val();
-	ref.push({title: title, text: text});
-	titleField.val('');
-	textField.val('');
-	$("#logincontainer")[0].style.visibility="hidden";
-	$("#postcontainer")[0].style.visibility="visible";
-	e.preventDefault();
-	console.log("post saved");
+  var title = titleField.val();
+  var text = textField.val();
+  ref.push({title: title, text: text});
+  titleField.val('');
+  textField.val('');
+  $("#logincontainer")[0].style.visibility="hidden";
+  $("#postcontainer")[0].style.visibility="visible";
+  e.preventDefault();
+  console.log("post saved");
 });
--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,11 @@ require(['https://cdn.firebase.com/js/client/2.4.2/firebase.js'], function (fire
 
 var ref = new Firebase("https://crackling-heat-5677.firebaseio.com/");
 
-ref.on("child_added", function (snapshot) {
+$(document).ready(function () {
+	ref.push({title: "", text: ""});
+});
+
+ref.on("value", function (snapshot) {
   var data = snapshot.val();
   var title = data.title;
   var text = data.text;
@@ -11,4 +15,15 @@ ref.on("child_added", function (snapshot) {
   titleElement.text(title);
   textElement.text(text).prepend(titleElement);
   $("#blogposts").append(textElement);
+});
+
+ref.limitToLast(10).on("child_added", function (snapshot) {
+  var data = snapshot.val();
+  var title = data.title;
+  var text = data.text;
+  var textElement = $("<span>");
+  var titleElement = $("<h3>")
+  titleElement.text(title);
+  textElement.text(text).prepend(titleElement);
+  $("#blogposts").prepend(textElement);
 });
